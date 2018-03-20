2ba14a4661a833ed4b9a36a3f8d51a2f1c0a8f1a
Mihail Paley <undeadlol1@mail.ru>
2018-03-20 18:48:29 +0500

    fix migration
    Signed-off-by: Mihail Paley <undeadlol1@mail.ru>

--- src/server/data/migrations/20180319135954-add-props-to-task.js
+++ src/server/data/migrations/20180319135954-add-props-to-task.js
@@ -13,7 +13,6 @@ module.exports = {
     )
     .then(() => queryInterface.addColumn('tasks', 'endTime', Sequelize.DATE))
     .then(() => queryInterface.addColumn('tasks', 'startTime', Sequelize.DATE))
-    .then(() => queryInterface.addColumn('tasks', 'interval', Sequelize.STRING))
   },
 
   down: (queryInterface, Sequelize) => {
@@ -21,6 +20,5 @@ module.exports = {
       .removeColumn('tasks', 'isBacktest')
       .then(() => queryInterface.removeColumn('tasks', 'endTime'))
       .then(() => queryInterface.removeColumn('tasks', 'startTime'))
-      .then(() => queryInterface.removeColumn('tasks', 'interval', Sequelize.STRING))
   }
 };
